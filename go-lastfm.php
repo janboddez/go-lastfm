2c45b5ce1884ec08e21e72bc72466dd74af7b742
Jan Boddez <jan.boddez@gmail.com>
2017-10-07 13:31:11 +0200

    Added more robust default styles, worked around compilation album limitations, bumped subversion no.

--- go-lastfm.php
+++ go-lastfm.php
@@ -3,7 +3,7 @@
  * Plugin Name: GO Last.fm
  * Plugin URI: https://janboddez.be/
  * Description: Display the (album) cover art to the tracks you most recently listened to. 
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Jan Boddez
  * Author URI: https://janboddez.be/
  */
@@ -18,6 +18,7 @@ class GO_Lastfm_Widget extends WP_Widget {
 			__( 'Last.fm Widget', 'go_lastfm' ), 
 			array( 'description' => __( 'Display the (album) cover art to the tracks you most recently listened to.', 'go_lastfm' ) )
 		);
+
 		if ( is_active_widget( false, false, $this->id_base, true ) ) {
 			wp_enqueue_style( 'go-lastfm', plugins_url( 'go-lastfm/go-lastfm.css' ) );
 		}
@@ -28,14 +29,18 @@ class GO_Lastfm_Widget extends WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		$title = apply_filters( 'widget_title', $instance['title'] );
+
 		echo $args['before_widget'];
+
 		if ( ! empty( $title ) ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
+
 		/**
 		 * Retrieves the album list stored previously
 		 */
-		$albums = get_option( 'go_lastfm_recent_albums' ); // var_dump( $albums );
+		$albums = get_option( 'go_lastfm_recent_albums' );
+
 		if ( ! empty( $albums ) && is_array( $albums ) ) {
 			/**
 			 * Loops through the list and displays the cover art and links to Last.fm
@@ -79,6 +84,7 @@ class GO_Lastfm_Widget extends WP_Widget {
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+
 		return $instance;
 	}
 }
@@ -92,9 +98,11 @@ class GO_Lastfm {
 	 */
 	public function __construct() {
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+		add_action( 'admin_menu', array( $this, 'create_menu' ) );
 		add_action( 'widgets_init', array( $this, 'load_widget' ) );
 		add_action( 'daily_load_album_list', array( $this, 'load_album_list' ) );
-		add_action( 'admin_menu', array( $this, 'create_menu' ) );
 	}
 
 	/**
@@ -102,11 +110,15 @@ class GO_Lastfm {
 	 */
 	public function activate() {
 		$timestamp = wp_next_scheduled( 'daily_load_album_list' );
-		if( false === $timestamp ) {
+
+		if ( false === $timestamp ) {
 			wp_schedule_event( time(), 'daily', 'daily_load_album_list' );
 		}
 	}
 
+	public function deactivate() {
+		wp_clear_scheduled_hook( 'daily_load_album_list' );
+	}
 	/**
 	 * Actually registers the widget defined above
 	 */
@@ -120,26 +132,41 @@ class GO_Lastfm {
 	public function load_album_list() {
 		$lastfm_api_key = get_option( 'go_lastfm_api_key' );
 		$lastfm_user_name = get_option( 'go_lastfm_user_name' );
+
+		$albums = array();
+
 		$response = @file_get_contents( 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' . $lastfm_user_name . '&limit=200&api_key=' . $lastfm_api_key . '&format=json' );
-		if ( $this->is_valid_json( $response ) ) {
-			$json = json_decode( $response, true ); // Returns an associative array
-			$albums = array();
-			foreach( $json['recenttracks']['track'] as $track ) {
-				$title = $track['artist']['#text'] . ' - ' . $track['album']['#text']; // 'Artist - Album Title'
+		$json = @json_decode( $response, true ); // Returns an associative array
+
+		if ( is_array( $json['recenttracks']['track'] ) ) {
+			foreach ( $json['recenttracks']['track'] as $track ) {
+				$title = /* $track['artist']['#text'] . ' - ' . */ trim( $track['album']['#text'] );
 				$thumbnail = $track['image'][2]['#text']; // Album cover URI
 				$mbid = $track['album']['mbid'];
-				if ( ! $this->in_album_list( $thumbnail, $albums ) ) {
+
+				// if ( ! $this->in_album_list( $thumbnail, $albums ) ) { // Won't work for compilation albums, as thumbnails aren't unique
+				if ( ! $this->in_album_list( $title, $albums ) ) {
 					$uri = '';
-					$response = @file_get_contents( 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=' . $mbid . '&api_key=' . $lastfm_api_key . '&format=json' );
-					if ( $this->is_valid_json( $response ) ) {
-						$json = json_decode( $response, true ); // Returns an associative array
-						$uri = $json['album']['url'];
+
+					if ( ! empty( $mbid ) ) {
+						$response = @file_get_contents( 'http://ws.audioscrobbler.com/2.0/?method=album.getinfo&mbid=' . $mbid . '&api_key=' . $lastfm_api_key . '&format=json' );
+						$json = @json_decode( $response, true ); // Returns an associative array
+
+						if ( ! empty( $json['album']['url'] ) /* && filter_var( $json['album']['url'], FILTER_VALIDATE_URL ) */ ) {
+							$uri = esc_url_raw( $json['album']['url'] );
+						}
 					}
+
 					$albums[] = array(
 						'title' => $title,
 						'uri' => $uri,
 						'thumbnail' => $thumbnail,
 					);
+				}
+
+				// Limits the album list to 8
+				if ( count( $albums ) >= 8 ) {
+					break;
 				}
 			}
 			/**
@@ -153,13 +180,7 @@ class GO_Lastfm {
 	 * Registers the plugin settings page
 	 */
 	public function create_menu() {
-		add_options_page(
-			'GO Last.fm', 
-			'GO Last.fm', 
-			'manage_options', 
-			'go-lastfm-settings', 
-			array( $this, 'settings_page' )
-		);
+		add_options_page( 'GO Last.fm', 'GO Last.fm', 'manage_options', 'go-lastfm-settings', array( $this, 'settings_page' ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
 	}
 
@@ -174,40 +195,33 @@ class GO_Lastfm {
 	public function settings_page() {
 	?>
 	<div class="wrap">
-	<h1>GO Last.fm</h1>
-	<form method="post" action="options.php">
-		<?php settings_fields( 'go-lastfm-settings-group' ); ?>
-		<?php do_settings_sections( 'go-lastfm-settings-group' ); ?>
-		<table class="form-table">
-			<tr valign="top">
-				<th scope="row">Last.fm API Key</th>
-				<td><input class="widefat" type="text" name="go_lastfm_api_key" value="<?php echo esc_attr( get_option( 'go_lastfm_api_key' ) ); ?>" /></td>
-			</tr>
-			<tr valign="top">
-				<th scope="row">Last.fm User Name</th>
-				<td><input class="widefat" type="text" name="go_lastfm_user_name" value="<?php echo esc_attr( get_option( 'go_lastfm_user_name' ) ); ?>" /></td>
-			</tr>
-		</table>
-		<?php submit_button(); ?>
-	</form>
+		<h1>GO Last.fm</h1>
+		<form method="post" action="options.php">
+			<?php settings_fields( 'go-lastfm-settings-group' ); ?>
+			<?php do_settings_sections( 'go-lastfm-settings-group' ); ?>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">Last.fm API Key</th>
+					<td><input class="widefat" type="text" name="go_lastfm_api_key" value="<?php echo esc_attr( get_option( 'go_lastfm_api_key' ) ); ?>" /></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Last.fm User Name</th>
+					<td><input class="widefat" type="text" name="go_lastfm_user_name" value="<?php echo esc_attr( get_option( 'go_lastfm_user_name' ) ); ?>" /></td>
+				</tr>
+			</table>
+			<?php submit_button(); ?>
+		</form>
 	</div>
 	<?php
 	}
 
-	/**
-	 * Helper functions
-	 */
-	private function is_valid_json( $str ) { 
-		json_decode( $str );
-		return ( json_last_error() === JSON_ERROR_NONE );
-	}
-
-	private function in_album_list( $thumbnail, $albums ) {
+	private function in_album_list( $title, $albums ) {
 		foreach ( $albums as $album ) {
-			if ( $album['thumbnail'] === $thumbnail ) {
+			if ( sanitize_title( $album['title'] ) === sanitize_title( $title ) ) {
 				return true;
 			}
 		}
+
 		return false;
 	}
 }
